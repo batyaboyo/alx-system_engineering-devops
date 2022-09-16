505bd94fd683922afdc78747c4a89667f32e286b
devbxtzz <batztonnie@gmail.com.com>
2022-09-16 17:13:21 +0300

    Small Updates

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# Installs puppet-lint 2.5.0 using puppet
-package {'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pup
-# This manifest kills a process named killmenow.
-exec { 'pkill':
-  command => 'pkill killmenow',
-  path    => '/usr/local/bin/:/bin/',
-}
