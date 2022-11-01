3854d291e53fe40cb817776d54b0915884541f8f
devbxtzz <batztonnie@gmail.com>
2022-11-01 14:45:08 +0300

    Parses the title of all hot articles

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,118 +1,52 @@
 #!/usr/bin/python3
-""" Recursive function that queries the Reddit API, parses the title of all
-hot articles, and prints a sorted count of given keywords (case-insensitive,
-delimited by spaces. Javascript should count as javascript,
-but java should not)
-"""
+"""Top ten"""
 import requests
 
 
-def f_after(subreddit, hot_list, after=""):
-    """Recursive function find to length of hot_list
-    Args:
-        subreddit: Account to search
-        hot_list: hot list of Reddit
-        after: next page of API
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(subreddit)
-    if after is not None:
-        data = requests.get(url,
-                            headers={'User-Agent': 'AgentMEGO'},
-                            params={'after': after})
-        if data.status_code != 200:
-            return None
-        after = data.json().get('data').get('after')
-        JSON = data.json().get('data').get('children')
-        for value in JSON:
-            hot_list.append(value.get('data').get('title'))
-        return f_after(subreddit, hot_list, after)
-    return (hot_list)
+def count_words(subreddit, word_list, after="", word_dic={}):
+    """Return a list of titles of all hot articles"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'user-agent': 'MyAPI/0.0.1'}
+    params = {'after': after}
 
+    if not word_dic:
+        for word in word_list:
+            word_dic[word] = 0
 
-def sort_dict_by_value(d, reverse=False):
-    return dict(sorted(
-        d.items(), key=lambda x: x[1], reverse=reverse))
-
-
-def count_words(subreddit, word_list):
-    """Returns a list containing the titles of all hot articles for
-    a given subreddit. If no results are found for the given subreddit,
-    the function should return None.
-
-    Args:
-        subreddit: Account to search
-    """
-    if subreddit is None or type(subreddit) is not str:
+    if after is None:
+        word_list = [[key, value] for key, value in word_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
         return None
 
-    data = f_after(subreddit, [], "")
-    data = " ".join(data)
-    data = data.split(" ")
+    r = requests.get(url, headers=headers, params=params,
+                     allow_redirects=False)
 
-    data_list = {}
-    num = 0
+    if r.status_code != 200:
+        return None
 
-    for i in word_list:
-        """
-        search = [char for char in i]
-        re_search = ""
-        for word in search:
-            if word.isalpha():
-                re_search += "[{}{}]".format(word.lower(), word.upper())
-            else:
-                re_search += word
-        for val in data:
-            text = re.compile("\\W" + re_search + "\\W")
-            if text.findall(val) != []:
-        num += 1
-        """
-        for val in data:
-            i = i.lower()
-            if val.lower() == i:
-                num += 1
-        if i in data_list.keys():
-            data_list[i] = data_list[i] + num
-        else:
-            data_list[i] = num
-        num = 0
+    try:
+        js = r.json()
 
-    new_data = data_list
-    tmp_key = ""
-    tmp_value = 0
-    char1 = []
-    char2 = []
-    x = 0
-    newnew = {}
-    new_data = sort_dict_by_value(new_data, True)
-    for key, value in new_data.items():
-        if value == 0:
-            continue
-        newnew[key] = value
-        if value == tmp_value:
-            char1 = [char for char in key]
-            char2 = [charr for charr in tmp_key]
-            c1 = ord(char1[x])
-            c2 = ord(char2[x])
-            c3 = 0
-            while (c1 and c2):
-                c1 = ord(char1[x])
-                c2 = ord(char2[x])
-                if c1 < c2:
-                    c3 = 0
-                    break
-                elif c1 > c2:
-                    c3 = 1
-                    break
-                x += 1
-            if c3 == 0:
-                del newnew[tmp_key]
-                newnew[key] = value
-                newnew[tmp_key] = tmp_value
-                tmp_key = tmp_key
-                tmp_value = tmp_value
-                continue
-        tmp_key = key
-        tmp_value = value
+    except ValueError:
+        return None
 
-    for a, b in newnew.items():
-        print("{}: {}".format(a, b))
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dic[w] += lower.count(w.lower())
+
+    except Exception as e:
+        return None
+
+    count_words(subreddit, word_list, after, word_dic)
